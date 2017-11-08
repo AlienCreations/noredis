f6f5415be8fd2c8a50828faf7cefceb4f8516def
Sean Cannon <alienwebguy@gmail.com>
2017-11-08 11:37:12 -0800

    2.1.0 - convert to es6 and add support for .on() and .quit()

--- spec/onSpec.js
+++ spec/onSpec.js
@@ -24,5 +24,35 @@ describe('on event', () => {
     expect(redisClient.on({a : 1}, _callback)).toBe('OK');
   });
 
+  it('fires a bound error event on an error', () => {
+    redisClient.on('error', () => redisClient.quit());
+    spyOn(redisClient, 'quit').and.callThrough();
+
+    // Trigger an error
+    redisClient.get(null);
+
+    expect(redisClient.quit).toHaveBeenCalled();
+  });
+
+  it('allows binding to an event multiple times', () => {
+
+    const callbacks = {
+      foo : () => {},
+      bar : () => {}
+    };
+
+    spyOn(callbacks, 'foo').and.callThrough();
+    spyOn(callbacks, 'bar').and.callThrough();
+
+    redisClient.on('error', () => callbacks.foo());
+    redisClient.on('error', () => callbacks.bar());
+
+    // Trigger an error
+    redisClient.get(null);
+
+    expect(callbacks.foo).toHaveBeenCalled();
+    expect(callbacks.bar).toHaveBeenCalled();
+
+  });
 });
 
